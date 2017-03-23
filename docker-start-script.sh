eede98b316ef09c431c4961e214fdc89e1f9df7d
Daniel Gepp <e1228976@student.tuwien.ac.at>
2017-03-23 12:16:04 +0100

    editing docker start script

--- docker-start-script.sh
+++ docker-start-script.sh
@@ -1,16 +1,9 @@
-ls
 cd /usr/src/app/
 gulp client.build:dist
-mkdir -p /usr/src/tmp/client
-cp -R /usr/src/app/server/ /usr/src/tmp
-cp -R /usr/src/app/tasks/ /usr/src/tmp
-cp /usr/src/app/gulpfile.babel.js /usr/src/tmp
-cp -R /usr/src/app/client/dist /usr/src/tmp/client
+cp -R /usr/src/app/. /usr/src/tmp
 rm -rf /usr/src/app/client/dist
 cd /usr/src/tmp/
 npm install
-cd /usr/src/app/
-npm install
 /usr/bin/mongod &
-gulp &
-npm start
+npm start &
+gulp
