06da7be44607412efb32a677de33e2eb87f66b91
Michael Eder <e1325852@student.tuwien.ac.at>
2017-04-27 16:24:50 +0200

    so ober jz.

--- server/api/controller/testdataController.js
+++ server/api/controller/testdataController.js
@@ -15,24 +15,24 @@ module.exports = class TestDataController {
       console.log("testdata");
       var crypto = require('crypto');
       let user1 = new User();
-      let bankAccount1;
+      let bankAccount1 = {};
       bankAccount1.balance = 300;
-      bankAccount1.iban = "AT55 7989 9877 9879"
-      let transaction;
+      bankAccount1.iban = "AT55 7989 9877 9879";
+      let transaction = {};
       transaction.date = new Date(2017, 4, 20, 10, 10, 10, 0);
       transaction.value = 700;
       transaction.ibanSender = "AT55 7989 9877 9879";
       transaction.ibanReceiver = "AT55 2189 1241 0275";
       transaction.paymentReference = "Ich need money.";
       transaction.category = "House";
-      let transaction2;
+      let transaction2 = {};
       transaction2.date = new Date(2017, 4, 20, 10, 10, 10, 0);
       transaction2.value = 1000;
       transaction2.ibanSender = "AT55 7989 9877 9879";
       transaction2.ibanReceiver = "AT55 2189 1241 0275";
       transaction2.paymentReference = "Ich need more money.";
       transaction2.category = "Car";
-      let transaction9;
+      let transaction9 = {};
       transaction9.date = new Date(2017, 4, 20, 10, 10, 10, 0);
       transaction9.value = 1000;
       transaction9.ibanSender = "AT55 7989 9877 9879";
@@ -47,27 +47,27 @@ module.exports = class TestDataController {
       user1.password = crypto.createHash('sha256').update('password').digest('hex');
 
       let user2 = new User();
-      let bankAccount2;
+      let bankAccount2 = {};
       bankAccount2.balance = 700;
-      bankAccount2.iban = "AT55 2189 1241 0275"
+      bankAccount2.iban = "AT55 2189 1241 0275";
       user2.username = 'gabi.musterfrau';
       user2.firstName = 'Gabi';
       user2.lastName = 'Musterfrau';
       user2.bankAccount = bankAccount2;
-      let transaction3;
+      let transaction3 = {};
       transaction3.date = new Date(2017, 4, 20, 10, 10, 10, 0);
       transaction3.ibanSender = "AT55 7989 9877 9879";
       transaction3.ibanReceiver = "AT55 2189 1241 0275";
       transaction3.paymentReference = "Ich need money.";
       transaction3.category = "House";
-      let transaction4;
+      let transaction4 = {};
       transaction4.date = new Date(2017, 4, 20, 10, 10, 10, 0);
       transaction4.value = 1000;
       transaction4.ibanSender = "AT55 7989 9877 9879";
       transaction4.ibanReceiver = "AT55 2189 1241 0275";
       transaction4.paymentReference = "Ich need more money.";
       transaction4.category = "Car";
-      let transaction5;
+      let transaction5 = {};
       transaction5.date = new Date(2017, 4, 20, 10, 10, 10, 0);
       transaction5.value = 1000;
       transaction5.ibanSender = "AT55 2189 1241 0275";
@@ -78,14 +78,14 @@ module.exports = class TestDataController {
       user2.password = crypto.createHash('sha256').update('password').digest('hex');
 
       let user3 = new User();
-      let bankAccount3;
+      let bankAccount3 = {};
       bankAccount3.balance = 1000;
-      bankAccount3.iban = "AT55 2189 1241 1111"
+      bankAccount3.iban = "AT55 2189 1241 1111";
       user3.username = 'erika.test';
       user3.firstName = 'Erika';
       user3.lastName = 'Test';
       user3.bankAccount = bankAccount3;
-      let transaction8;
+      let transaction8 = {};
       transaction8.date = new Date(2017, 4, 20, 10, 10, 10, 0);
       transaction8.value = 1000;
       transaction8.ibanSender = "AT55 2189 1241 1111";
@@ -93,7 +93,7 @@ module.exports = class TestDataController {
       transaction8.paymentReference = "Fun.";
       transaction8.category = "Fun";
 
-      let transaction10;
+      let transaction10 = {};
       transaction10.date = new Date(2017, 4, 20, 10, 10, 10, 0);
       transaction10.value = 1000;
       transaction10.ibanSender = "AT55 2189 1241 1111";
