16ae2554d9269fc70c02270161b8ca926d7aaadc
jobrot <jobrot94@gmail.com>
2017-05-13 22:33:59 +0200

    check webdriver

---
+++ tests/selenium/selenium-webdriver-simple.js
@@ -0,0 +1,25 @@
+var webdriver = require('selenium-webdriver'),
+  username = "jobrot94",
+  accessKey = "ab4eda0a-c39c-4107-b8d3-1a3ab459d16f",
+  driver;
+
+driver = new webdriver.Builder().
+withCapabilities({
+  'browserName': 'chrome',
+  'platform': 'Windows XP',
+  'version': '43.0',
+  'username': username,
+  'accessKey': accessKey
+}).
+usingServer("http://" + username + ":" + accessKey +
+  "@ondemand.saucelabs.com:80/wd/hub").
+build();
+
+driver.get("http://saucelabs.com/test/guinea-pig");
+
+driver.getTitle().then(function (title) {
+  console.log("title is: " + title);
+  throw "This should break the build";
+});
+
+driver.quit();
