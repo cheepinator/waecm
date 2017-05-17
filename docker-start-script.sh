dd310923e2a8dc83241d0ad1c3f48a7376334e00
jobrot <jobrot94@gmail.com>
2017-05-17 13:34:22 +0200

    removing sauce in server and forwarding from container

--- docker-start-script.sh
+++ docker-start-script.sh
@@ -51,28 +51,6 @@ if [ -n "$1" -a \( "$1" = "build" -o "$1" = "deploy" -o "$1" = "test" -o "$1" = 
             npm install -g bower
             npm install
             typings install
-            echo "starting sauce..."
-
-            CONNECT_URL="http://saucelabs.com/downloads/Sauce-Connect-latest.zip"
-            CONNECT_DIR="/tmp/sauce-connect-$RANDOM"
-            CONNECT_DOWNLOAD="Sauce_Connect.zip"
-            READY_FILE="connect-ready-$RANDOM"
-
-            # Get Connect and start it
-            mkdir -p $CONNECT_DIR
-            cd $CONNECT_DIR
-            curl $CONNECT_URL > $CONNECT_DOWNLOAD
-            unzip $CONNECT_DOWNLOAD
-            rm $CONNECT_DOWNLOAD
-            java -jar Sauce-Connect.jar --readyfile $READY_FILE \
-                --tunnel-identifier $TRAVIS_JOB_NUMBER \
-                $SAUCE_USERNAME $SAUCE_ACCESS_KEY &
-
-            # Wait for Connect to be ready before exiting
-            while [ ! -f $READY_FILE ]; do
-              sleep .5
-            done
-            echo "sauce started"
             #export DISPLAY=:99.0
             #Xvfb :99 -screen 0 640x480x8 -nolisten tcp
             gulp client.build:dist
